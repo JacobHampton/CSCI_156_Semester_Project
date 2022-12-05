d6e3049b42a387331e051c3c4b3dad301ebbd0bf
mattyhatch <mattyhatch6@gmail.com>
2022-12-05 09:46:48 -0800

    Added functionality for car script Made the start of the server, race runs when 3 cars are connected to server

--- car.py
+++ car.py
@@ -1,1 +1,20 @@
 #Cars with transponders
+import socket
+import time
+import random
+import struct
+
+#Replace with your ip address when you want to test it
+address = '129.8.226.210'
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((address, 8080))
+flag = True
+while flag:
+    from_server = client.recv(4096)
+    if from_server == b"Run":
+        flag = False
+for i in range(5):
+    random1 = round(random.uniform(13.00,16.00),2)
+    time.sleep(random1)
+    client.send(bytearray(struct.pack("f",random1)))
+client.close()
--- server.py
+++ server.py
@@ -1,1 +1,30 @@
 #Server app
+import socket
+import _thread
+import time
+import struct
+#Currently have it set up to where it starts the race when 3 cars are connected
+#This can be changed by changing the car limit variable
+car_limit = 3
+#Replace with your ip address when you want to test it
+address = '129.8.226.210'
+counter = 0
+def newClient(conn, addr):
+    while counter < car_limit:
+        time.sleep(0.001)
+    conn.send(b"Run")
+    while True:
+        data = conn.recv(4096)
+        if not data:
+            break
+        print(str(addr)+":"+str(struct.unpack("f",data)))
+    conn.close()
+
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv.bind((address, 8080))
+serv.listen(3)
+while True:
+    conn, addr = serv.accept()
+    print("New Connection!")
+    counter += 1
+    _thread.start_new_thread(newClient,(conn,addr))
