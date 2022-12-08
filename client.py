8477e1878a3648398ea5ffab8542c8e5fbc1c5a9
Johnny-Dean <73153392+Johnny-Dean@users.noreply.github.com>
2022-12-07 21:13:09 -0800

    Fix - Update variable names, add message count, and remove unneeded comments

--- client.py
+++ client.py
@@ -1,4 +1,3 @@
-# Import the necessary modules
 import threading
 import tkinter as tk
 from tkinter import font
@@ -9,40 +8,38 @@ address = '192.168.1.245'
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((address, 8080))
 client.send(b'client')
+total_times = 0
 
-# Create a Tkinter window
-root = tk.Tk()
-my_font = font.Font(family='Helvetica', size=20)
-my_header_font = font.Font(family='Helvetica', size=20, weight='bold')
-# Add a label to the window
-title = tk.Label(root, text='Times:', font=my_header_font)
-label = tk.Label(root, text='Waiting for data...', font=my_font)
+window = tk.Tk()
+window.title("TCP Client") 
+running_font = font.Font(family='Helvetica', size=20)
+header_font = font.Font(family='Helvetica', size=20, weight='bold')
+# Add the text to the window (for some reason tkinter calls the text labels)
+title = tk.Label(window, text='0 Total Times Recorded:', font=header_font)
+messages = tk.Label(window, text='Waiting for data...', font=running_font)
 title.pack()
-label.pack()
+messages.pack()
 
 # Create a function that will be called when data is received
 def update_label(data):
   # Update the text of the label
-  if label['text'] == 'Waiting for data...':
-    label['text'] =  data + '\n'
+  if messages['text'] == 'Waiting for data...':
+    messages['text'] =  data + '\n'
   else:
-    label['text'] =  label['text'] + data + '\n'
+    messages['text'] =  messages['text'] + data + '\n'
+  title['text'] = str(total_times) + " Total Times Recorded:"
 
-# Create a function that will run in a separate thread and handle the socket
-# communication
-def socket_thread():
-  # Set up a loop that will receive data from the server and call the update_label
-  # function when new data is received
+# Create a function that will run in a separate thread and handle the socket while loop / recieving
+def newSocket():
+  global total_times
   while True:
     from_server = client.recv(4096)
     # Update the label with the received data
+    total_times += 1
     update_label(from_server.decode())
 
 # Create a new thread for the socket communication
-thread = threading.Thread(target=socket_thread)
+thread = threading.Thread(target=newSocket)
 
-# Start the thread
 thread.start()
-
-# Start the Tkinter event loop
-root.mainloop()
+window.mainloop()
